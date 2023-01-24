af96929f6a52800dca97ba2000b873426fdaf5a8
icecompanysas <54846370+icecompanysas@users.noreply.github.com>
2023-01-24 10:03:16 -0500

    proyecto android admin

---
+++ vorder-admin/App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
+import WebView from 'react-native-webview';
+import { View, StatusBar } from 'react-native';
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [])
+  return(
+  <View style= {{flex: 1}}>
+    <StatusBar barStyle= "dark-content" backgroundColor={'#ffffff'} />
+    <WebView source={{uri: 'https://www.v-order.com.co'}} style = {{marginTop: 20}} />
+    </View>
+  );
+}
+export default App;
